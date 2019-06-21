249b417ff0639e85aaff9ec80b89c9f09ae7dd60
Harshit Srivastava <harshit12893@live.com>
2019-06-21 21:50:35 +0530

    Initial commit - project setup

---
+++ src/main/java/com/url/urlshortener/App.java
@@ -0,0 +1,13 @@
+package com.url.urlshortener;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/url/urlshortener/model/UrlsData.java
@@ -0,0 +1,5 @@
+package com.url.urlshortener.model;
+
+public class UrlsData {
+	
+}
