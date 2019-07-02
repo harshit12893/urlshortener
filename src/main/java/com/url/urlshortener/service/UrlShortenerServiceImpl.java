a90a78c596b0300cafc5a1d06c0c0f77b7c7018d
Harshit Srivastava <harshit12893@live.com>
2019-07-02 22:08:05 +0530

    Dummy Service class and methods added

---
+++ src/main/java/com/url/urlshortener/service/UrlShortenerServiceImpl.java
@@ -0,0 +1,12 @@
+package com.url.urlshortener.service;
+
+import com.url.urlshortener.beans.UrlRequestBean;
+
+public class UrlShortenerServiceImpl implements UrlShortenerService {
+
+	public String shortenUrl(UrlRequestBean urlRequestBean) {
+		// TODO Main coding logic will go here
+		return null;
+	}
+
+}
