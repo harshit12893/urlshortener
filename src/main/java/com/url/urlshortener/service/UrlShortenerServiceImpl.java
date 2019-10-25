125b541fd3b19cfd13cc2d6ff72a38598812fde8
harshit12893 <harshit12893@live.com>
2019-10-25 16:53:50 +0530

    Logic checking test

--- src/main/java/com/url/urlshortener/service/UrlShortenerServiceImpl.java
+++ src/main/java/com/url/urlshortener/service/UrlShortenerServiceImpl.java
@@ -1,25 +1,47 @@
 package com.url.urlshortener.service;
 
+import java.util.Date;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.url.urlshortener.beans.UrlRequestBean;
+import com.url.urlshortener.dao.UrlShortenerDao;
+import com.url.urlshortener.model.UrlsData;
+import com.url.urlshortener.service.UrlShortenerService;
 
 public class UrlShortenerServiceImpl implements UrlShortenerService {
+
+
+@Autowired
+UrlShortenerDao urlShortenerDao;
 
 	public static final String base62Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRS"
 			+ "TUVWXYZ0123456789";
 	public static final int BASE = base62Chars.length();
 	public String shortenUrl(UrlRequestBean urlRequestBean) {
 		// TODO Main coding logic will go here
-		return null;
+		Date date = new Date();
+		UrlsData  urlData= new UrlsData();
+		urlData.setCreatedDate(date);
+		
+		String oldUrl = urlRequestBean.getOrginalUrl();
+		Random rand = new Random();
+		long randomNumber = Math.abs(rand.nextLong());
+		System.out.println("Random number " +randomNumber);
+		String base62Num = convertBase10ToBase62(randomNumber);
+		return base62Num;
+//		return null;
 	}
 	
-	public String convertBase10ToBase62(long inputNumber) {
+	public static String convertBase10ToBase62(long inputNumber) {
 
 		StringBuilder base62=new StringBuilder("");
 		if(inputNumber < 0 )
 			return "";
 		try {
 		while(inputNumber > 0) {
-			int digit=(int)inputNumber%BASE;
+			int digit=(int) (inputNumber%BASE);
 			base62.append(base62Chars.charAt(digit));
 			inputNumber/=BASE;
 		}
@@ -41,5 +63,15 @@ public class UrlShortenerServiceImpl implements UrlShortenerService {
 			result+=index*(long)Math.pow(BASE, i);
 		}
 		return result;
+	}
+	
+	public static void main(String ar[]) {
+		UrlRequestBean bean= new UrlRequestBean();
+		UrlShortenerServiceImpl imp= new UrlShortenerServiceImpl();
+		bean.setOrginalUrl("https://stackoverflow.com/questions/5887709/getting-random-numbers-in-java");
+		String shorter = imp.shortenUrl(bean);
+		long backTo = imp.convertBase62ToBase10(shorter);
+		System.out.println(shorter);
+		System.out.println(backTo);
 	}
 }
